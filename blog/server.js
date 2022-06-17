f2def8d5d2eae231750f702ff1820a955d6336a0
WatafacRumeinia <cosminflorinel23@gmail.com>
2022-06-17 19:39:42 +0300

    Learning JS

---
+++ blog/server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const  articleRouter=require('./routes/articles')
+const app = express()
+
+app.set('view engine', 'ejs')
+
+app.use('/articles', articleRouter)
+
+app.get('/' , (req, res)=>{
+    const articles = [{
+        title: "Test Article",
+        createdAt: new Date,
+        description:"Test Description"
+    },
+    {
+        title: "Test Article 2",
+        createdAt: new Date,
+        description:"Test Description"
+    }]
+    res.render('articles/index', {articles:articles})
+})
+
+app.listen(5000)
