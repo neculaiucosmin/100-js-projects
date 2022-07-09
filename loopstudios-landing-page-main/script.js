c8d98c8d4e35faa4032bddef9f0b99122567c213
cosminneculaiu <cosminflorinel23@gmail.com>
2022-07-09 19:50:43 +0300

    Desktop css update

--- loopstudios-landing-page-main/script.js
+++ loopstudios-landing-page-main/script.js
@@ -3,6 +3,7 @@ const navList=document.querySelector(".nav-bar");
 const mainContainer=document.querySelector(".main")
 const ftr=document.querySelector(".footer")
 const navLogo=document.querySelector(".nav-logo")
+const title=document.querySelector(".ttl")
 
 toggleBtn.addEventListener("click", ()=>{
     if (toggleBtn.classList.contains("active")){
@@ -10,6 +11,7 @@ toggleBtn.addEventListener("click", ()=>{
         navList.classList.remove("active");
         navLogo.classList.remove("active");
         mainContainer.classList.remove("hidden");
+        title.classList.remove("hidden");
         ftr.classList.remove("hidden");
     }
     else {
@@ -17,6 +19,7 @@ toggleBtn.addEventListener("click", ()=>{
         navList.classList.add("active");
         navLogo.classList.add("active")
         mainContainer.classList.add("hidden");
+        title.classList.add("hidden");
         ftr.classList.add("hidden");
 
     }
