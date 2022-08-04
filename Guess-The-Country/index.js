af158d61f5c64d5f01554b12be49140665b72fee
cosminneculaiu <cosminflorinel23@gmail.com>
2022-08-05 00:24:12 +0300

    main menu page

--- Guess-The-Country/index.js
+++ Guess-The-Country/index.js
@@ -1,648 +1,34 @@
+const urlEu = "https://restcountries.com/v2/regionalbloc/eu";
+let json,arr
 
-const json=[
-    {
-        "name": "Yemen",
-        "topLevelDomain": [
-            ".ye"
-        ],
-        "alpha2Code": "YE",
-        "alpha3Code": "YEM",
-        "callingCodes": [
-            "967"
-        ],
-        "capital": "Sana'a",
-        "altSpellings": [
-            "YE",
-            "Yemeni Republic",
-            "al-Jumhūriyyah al-Yamaniyyah"
-        ],
-        "subregion": "Western Asia",
-        "region": "Asia",
-        "population": 29825968,
-        "latlng": [
-            15.0,
-            48.0
-        ],
-        "demonym": "Yemeni",
-        "area": 527968.0,
-        "gini": 36.7,
-        "timezones": [
-            "UTC+03:00"
-        ],
-        "borders": [
-            "OMN",
-            "SAU"
-        ],
-        "nativeName": "اليَمَن",
-        "numericCode": "887",
-        "flags": {
-            "svg": "https://flagcdn.com/ye.svg",
-            "png": "https://flagcdn.com/w320/ye.png"
-        },
-        "currencies": [
-            {
-                "code": "YER",
-                "name": "Yemeni rial",
-                "symbol": "﷼"
-            }
-        ],
-        "languages": [
-            {
-                "iso639_1": "ar",
-                "iso639_2": "ara",
-                "name": "Arabic",
-                "nativeName": "العربية"
-            }
-        ],
-        "translations": {
-            "br": "Iêmen",
-            "pt": "Iémen",
-            "nl": "Jemen",
-            "hr": "Jemen",
-            "fa": "یمن",
-            "de": "Jemen",
-            "es": "Yemen",
-            "fr": "Yémen",
-            "ja": "イエメン",
-            "it": "Yemen",
-            "hu": "Jemen"
-        },
-        "flag": "https://flagcdn.com/ye.svg",
-        "regionalBlocs": [
-            {
-                "acronym": "AL",
-                "name": "Arab League",
-                "otherNames": [
-                    "جامعة الدول العربية",
-                    "Jāmiʻat ad-Duwal al-ʻArabīyah",
-                    "League of Arab States"
-                ]
-            }
-        ],
-        "cioc": "YEM",
-        "independent": true
-    },
-    {
-        "name": "Zambia",
-        "topLevelDomain": [
-            ".zm"
-        ],
-        "alpha2Code": "ZM",
-        "alpha3Code": "ZMB",
-        "callingCodes": [
-            "260"
-        ],
-        "capital": "Lusaka",
-        "altSpellings": [
-            "ZM",
-            "Republic of Zambia"
-        ],
-        "subregion": "Eastern Africa",
-        "region": "Africa",
-        "population": 18383956,
-        "latlng": [
-            -15.0,
-            30.0
-        ],
-        "demonym": "Zambian",
-        "area": 752618.0,
-        "gini": 57.1,
-        "timezones": [
-            "UTC+02:00"
-        ],
-        "borders": [
-            "AGO",
-            "BWA",
-            "COD",
-            "MWI",
-            "MOZ",
-            "NAM",
-            "TZA",
-            "ZWE"
-        ],
-        "nativeName": "Zambia",
-        "numericCode": "894",
-        "flags": {
-            "svg": "https://flagcdn.com/zm.svg",
-            "png": "https://flagcdn.com/w320/zm.png"
-        },
-        "currencies": [
-            {
-                "code": "ZMW",
-                "name": "Zambian kwacha",
-                "symbol": "ZK"
-            }
-        ],
-        "languages": [
-            {
-                "iso639_1": "en",
-                "iso639_2": "eng",
-                "name": "English",
-                "nativeName": "English"
-            }
-        ],
-        "translations": {
-            "br": "Zâmbia",
-            "pt": "Zâmbia",
-            "nl": "Zambia",
-            "hr": "Zambija",
-            "fa": "زامبیا",
-            "de": "Sambia",
-            "es": "Zambia",
-            "fr": "Zambie",
-            "ja": "ザンビア",
-            "it": "Zambia",
-            "hu": "Zambia"
-        },
-        "flag": "https://flagcdn.com/zm.svg",
-        "regionalBlocs": [
-            {
-                "acronym": "AU",
-                "name": "African Union",
-                "otherNames": [
-                    "الاتحاد الأفريقي",
-                    "Union africaine",
-                    "União Africana",
-                    "Unión Africana",
-                    "Umoja wa Afrika"
-                ]
-            }
-        ],
-        "cioc": "ZAM",
-        "independent": true
-    },
-    {
-        "name": "Zimbabwe",
-        "topLevelDomain": [
-            ".zw"
-        ],
-        "alpha2Code": "ZW",
-        "alpha3Code": "ZWE",
-        "callingCodes": [
-            "263"
-        ],
-        "capital": "Harare",
-        "altSpellings": [
-            "ZW",
-            "Republic of Zimbabwe"
-        ],
-        "subregion": "Southern Africa",
-        "region": "Africa",
-        "population": 14862927,
-        "latlng": [
-            -20.0,
-            30.0
-        ],
-        "demonym": "Zimbabwean",
-        "area": 390757.0,
-        "gini": 50.3,
-        "timezones": [
-            "UTC+02:00"
-        ],
-        "borders": [
-            "BWA",
-            "MOZ",
-            "ZAF",
-            "ZMB"
-        ],
-        "nativeName": "Zimbabwe",
-        "numericCode": "716",
-        "flags": {
-            "svg": "https://flagcdn.com/zw.svg",
-            "png": "https://flagcdn.com/w320/zw.png"
-        },
-        "currencies": [
-            {
-                "code": "ZMW",
-                "name": "Zambian kwacha",
-                "symbol": "K"
-            }
-        ],
-        "languages": [
-            {
-                "iso639_1": "en",
-                "iso639_2": "eng",
-                "name": "English",
-                "nativeName": "English"
-            },
-            {
-                "iso639_1": "sn",
-                "iso639_2": "sna",
-                "name": "Shona",
-                "nativeName": "chiShona"
-            },
-            {
-                "iso639_1": "nd",
-                "iso639_2": "nde",
-                "name": "Northern Ndebele",
-                "nativeName": "isiNdebele"
-            }
-        ],
-        "translations": {
-            "br": "Zimbabwe",
-            "pt": "Zimbabué",
-            "nl": "Zimbabwe",
-            "hr": "Zimbabve",
-            "fa": "زیمباوه",
-            "de": "Simbabwe",
-            "es": "Zimbabue",
-            "fr": "Zimbabwe",
-            "ja": "ジンバブエ",
-            "it": "Zimbabwe",
-            "hu": "Zimbabwe"
-        },
-        "flag": "https://flagcdn.com/zw.svg",
-        "regionalBlocs": [
-            {
-                "acronym": "AU",
-                "name": "African Union",
-                "otherNames": [
-                    "الاتحاد الأفريقي",
-                    "Union africaine",
-                    "União Africana",
-                    "Unión Africana",
-                    "Umoja wa Afrika"
-                ]
-            }
-        ],
-        "cioc": "ZIM",
-        "independent": true
-    },
-    {
-        "name": "Afghanistan",
-        "topLevelDomain": [
-            ".af"
-        ],
-        "alpha2Code": "AF",
-        "alpha3Code": "AFG",
-        "callingCodes": [
-            "93"
-        ],
-        "capital": "Kabul",
-        "altSpellings": [
-            "AF",
-            "Afġānistān"
-        ],
-        "subregion": "Southern Asia",
-        "region": "Asia",
-        "population": 40218234,
-        "latlng": [
-            33.0,
-            65.0
-        ],
-        "demonym": "Afghan",
-        "area": 652230.0,
-        "timezones": [
-            "UTC+04:30"
-        ],
-        "borders": [
-            "IRN",
-            "PAK",
-            "TKM",
-            "UZB",
-            "TJK",
-            "CHN"
-        ],
-        "nativeName": "افغانستان",
-        "numericCode": "004",
-        "flags": {
-            "svg": "https://upload.wikimedia.org/wikipedia/commons/5/5c/Flag_of_the_Taliban.svg",
-            "png": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Flag_of_the_Taliban.svg/320px-Flag_of_the_Taliban.svg.png"
-        },
-        "currencies": [
-            {
-                "code": "AFN",
-                "name": "Afghan afghani",
-                "symbol": "؋"
-            }
-        ],
-        "languages": [
-            {
-                "iso639_1": "ps",
-                "iso639_2": "pus",
-                "name": "Pashto",
-                "nativeName": "پښتو"
-            },
-            {
-                "iso639_1": "uz",
-                "iso639_2": "uzb",
-                "name": "Uzbek",
-                "nativeName": "Oʻzbek"
-            },
-            {
-                "iso639_1": "tk",
-                "iso639_2": "tuk",
-                "name": "Turkmen",
-                "nativeName": "Türkmen"
-            }
-        ],
-        "translations": {
-            "br": "Afeganistão",
-            "pt": "Afeganistão",
-            "nl": "Afghanistan",
-            "hr": "Afganistan",
-            "fa": "افغانستان",
-            "de": "Afghanistan",
-            "es": "Afganistán",
-            "fr": "Afghanistan",
-            "ja": "アフガニスタン",
-            "it": "Afghanistan",
-            "hu": "Afganisztán"
-        },
-        "flag": "https://upload.wikimedia.org/wikipedia/commons/5/5c/Flag_of_the_Taliban.svg",
-        "regionalBlocs": [
-            {
-                "acronym": "SAARC",
-                "name": "South Asian Association for Regional Cooperation"
-            }
-        ],
-        "cioc": "AFG",
-        "independent": true
-    },
-    {
-        "name": "Åland Islands",
-        "topLevelDomain": [
-            ".ax"
-        ],
-        "alpha2Code": "AX",
-        "alpha3Code": "ALA",
-        "callingCodes": [
-            "358"
-        ],
-        "capital": "Mariehamn",
-        "altSpellings": [
-            "AX",
-            "Aaland",
-            "Aland",
-            "Ahvenanmaa"
-        ],
-        "subregion": "Northern Europe",
-        "region": "Europe",
-        "population": 28875,
-        "latlng": [
-            60.116667,
-            19.9
-        ],
-        "demonym": "Ålandish",
-        "area": 1580.0,
-        "timezones": [
-            "UTC+02:00"
-        ],
-        "nativeName": "Åland",
-        "numericCode": "248",
-        "flags": {
-            "svg": "https://flagcdn.com/ax.svg",
-            "png": "https://flagcdn.com/w320/ax.png"
-        },
-        "currencies": [
-            {
-                "code": "EUR",
-                "name": "Euro",
-                "symbol": "€"
-            }
-        ],
-        "languages": [
-            {
-                "iso639_1": "sv",
-                "iso639_2": "swe",
-                "name": "Swedish",
-                "nativeName": "svenska"
-            }
-        ],
-        "translations": {
-            "br": "Ilhas de Aland",
-            "pt": "Ilhas de Aland",
-            "nl": "Ålandeilanden",
-            "hr": "Ålandski otoci",
-            "fa": "جزایر الند",
-            "de": "Åland",
-            "es": "Alandia",
-            "fr": "Åland",
-            "ja": "オーランド諸島",
-            "it": "Isole Aland",
-            "hu": "Åland-szigetek"
-        },
-        "flag": "https://flagcdn.com/ax.svg",
-        "regionalBlocs": [
-            {
-                "acronym": "EU",
-                "name": "European Union"
-            }
-        ],
-        "independent": false
-    },
-    {
-        "name": "Albania",
-        "topLevelDomain": [
-            ".al"
-        ],
-        "alpha2Code": "AL",
-        "alpha3Code": "ALB",
-        "callingCodes": [
-            "355"
-        ],
-        "capital": "Tirana",
-        "altSpellings": [
-            "AL",
-            "Shqipëri",
-            "Shqipëria",
-            "Shqipnia"
-        ],
-        "subregion": "Southern Europe",
-        "region": "Europe",
-        "population": 2837743,
-        "latlng": [
-            41.0,
-            20.0
-        ],
-        "demonym": "Albanian",
-        "area": 28748.0,
-        "gini": 33.2,
-        "timezones": [
-            "UTC+01:00"
-        ],
-        "borders": [
-            "MNE",
-            "GRC",
-            "MKD",
-            "UNK"
-        ],
-        "nativeName": "Shqipëria",
-        "numericCode": "008",
-        "flags": {
-            "svg": "https://flagcdn.com/al.svg",
-            "png": "https://flagcdn.com/w320/al.png"
-        },
-        "currencies": [
-            {
-                "code": "ALL",
-                "name": "Albanian lek",
-                "symbol": "L"
-            }
-        ],
-        "languages": [
-            {
-                "iso639_1": "sq",
-                "iso639_2": "sqi",
-                "name": "Albanian",
-                "nativeName": "Shqip"
-            }
-        ],
-        "translations": {
-            "br": "Albânia",
-            "pt": "Albânia",
-            "nl": "Albanië",
-            "hr": "Albanija",
-            "fa": "آلبانی",
-            "de": "Albanien",
-            "es": "Albania",
-            "fr": "Albanie",
-            "ja": "アルバニア",
-            "it": "Albania",
-            "hu": "Albánia"
-        },
-        "flag": "https://flagcdn.com/al.svg",
-        "regionalBlocs": [
-            {
-                "acronym": "CEFTA",
-                "name": "Central European Free Trade Agreement"
-            }
-        ],
-        "cioc": "ALB",
-        "independent": true
-    },
-    {
-        "name": "Algeria",
-        "topLevelDomain": [
-            ".dz"
-        ],
-        "alpha2Code": "DZ",
-        "alpha3Code": "DZA",
-        "callingCodes": [
-            "213"
-        ],
-        "capital": "Algiers",
-        "altSpellings": [
-            "DZ",
-            "Dzayer",
-            "Algérie"
-        ],
-        "subregion": "Northern Africa",
-        "region": "Africa",
-        "population": 44700000,
-        "latlng": [
-            28.0,
-            3.0
-        ],
-        "demonym": "Algerian",
-        "area": 2381741.0,
-        "gini": 27.6,
-        "timezones": [
-            "UTC+01:00"
-        ],
-        "borders": [
-            "TUN",
-            "LBY",
-            "NER",
-            "ESH",
-            "MRT",
-            "MLI",
-            "MAR"
-        ],
-        "nativeName": "الجزائر",
-        "numericCode": "012",
-        "flags": {
-            "svg": "https://flagcdn.com/dz.svg",
-            "png": "https://flagcdn.com/w320/dz.png"
-        },
-        "currencies": [
-            {
-                "code": "DZD",
-                "name": "Algerian dinar",
-                "symbol": "د.ج"
-            }
-        ],
-        "languages": [
-            {
-                "iso639_1": "ar",
-                "iso639_2": "ara",
-                "name": "Arabic",
-                "nativeName": "العربية"
-            }
-        ],
-        "translations": {
-            "br": "Argélia",
-            "pt": "Argélia",
-            "nl": "Algerije",
-            "hr": "Alžir",
-            "fa": "الجزایر",
-            "de": "Algerien",
-            "es": "Argelia",
-            "fr": "Algérie",
-            "ja": "アルジェリア",
-            "it": "Algeria",
-            "hu": "Algéria"
-        },
-        "flag": "https://flagcdn.com/dz.svg",
-        "regionalBlocs": [
-            {
-                "acronym": "AU",
-                "name": "African Union",
-                "otherNames": [
-                    "الاتحاد الأفريقي",
-                    "Union africaine",
-                    "União Africana",
-                    "Unión Africana",
-                    "Umoja wa Afrika"
-                ]
-            },
-            {
-                "acronym": "AL",
-                "name": "Arab League",
-                "otherNames": [
-                    "جامعة الدول العربية",
-                    "Jāmiʻat ad-Duwal al-ʻArabīyah",
-                    "League of Arab States"
-                ]
-            }
-        ],
-        "cioc": "ALG",
-        "independent": true
-    },
-]
+async function getCountries() {
+    const response = await fetch(urlEu);
+    const data = await response.json();
+    addData(data)
+    arr = generateArray();
+    updateQuestion();
+    //generateArray();
+}
 
-const pickA=document.getElementById("a1");
-const pickB=document.getElementById("a2");
-const pickC=document.getElementById("a3");
-const pickD=document.getElementById("a4");
+function addData(obj) {
+    json = obj;
+
+    //console.log(json)
+}
+
+getCountries()
+
+const pickA = document.getElementById("a1");
+const pickB = document.getElementById("a2");
+const pickC = document.getElementById("a3");
+const pickD = document.getElementById("a4");
 const score = document.getElementById("correctAns");
 
 
-function random(inp){
+function random(inp) {
     let num;
-    num=Math.floor(Math.random()*inp);
+    num = Math.floor(Math.random() * inp);
     return num;
-}
-let res
-function setCorrectAns(currentNum){
-    const img=document.getElementById("flag");
-    img.src=json[currentNum]["flag"];
-    let variants=[pickA,pickB,pickC,pickD];
-    let pos = variants[random(3)]
-    for (let wrongAnsKey in variants) {
-        if (variants[wrongAnsKey]!==pos){
-            variants[wrongAnsKey].innerText=json[random(json.length)]["name"];
-        }
-    }
-
-    pos.innerText=json[currentNum]["name"];
-    document.querySelectorAll(".ans-wrapper").forEach(item=>{
-        item.addEventListener("click", ()=>{
-            if(item.textContent===pos.textContent)
-                return new Promise(()=> res=true)
-            return new Promise(() => res=false)
-        })
-    })
 }
 
 function generateArray() {
@@ -650,11 +36,51 @@ function generateArray() {
     for (let i = 0; i < 10; i++) {
         arr.push(random(json.length));
     }
-    let counter = 0;
-    for (let j in arr){
-        let res=setCorrectAns(arr[j])
-        console.log(res)
-        score.innerText=counter;
-    }
+    return arr;
 }
-generateArray();
+
+let questionNumber = 0;
+let counter = 0;
+let variants = [pickA, pickB, pickC, pickD];
+let pos = variants[random(3)]
+document.querySelectorAll(".ans-wrapper").forEach(item => {
+    item.addEventListener("click", () => {
+        if (item.textContent === pos.textContent) {
+            counter++;
+            if (questionNumber < 10) {
+                questionNumber++;
+                updateQuestion();
+            }
+        } else {
+            if (questionNumber < 10) {
+                questionNumber++;
+                updateQuestion();
+            }
+        }
+        score.innerText = counter;
+    });
+});
+
+function setCorrectAns(currentNum) {
+    console.log(json)
+    const img = document.getElementById("flag");
+    img.src = json[currentNum]["flag"];
+    for (let wrongAnsKey in variants) {
+        if (variants[wrongAnsKey] !== pos) {
+            variants[wrongAnsKey].innerText = json[random(json.length)]["name"];
+        }
+    }
+    pos.innerText = json[currentNum]["name"];
+}
+
+function updateQuestion() {
+    //console.log(arr[questionNumber])
+    console.log(questionNumber);
+    setCorrectAns(arr[questionNumber])
+}
+
+function startGame(){
+    
+}
+
+
